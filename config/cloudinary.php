907e1d02de432c5203401474b7c11d1f14e0b992
Marco <magp2606gmail.com>
2025-06-28 22:03:14 -0500

    Configurar cloudinary

---
+++ config/cloudinary.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once 'config/env_var.php';
+
+use Cloudinary\Configuration\Configuration;
+use Cloudinary\Api\Upload\UploadApi;
+
+Configuration::instance([
+  'cloud' => [
+    'cloud_name' => CLOUDINARY_CLOUD_NAME,
+    'api_key' => CLOUDINARY_API_KEY,
+    'api_secret' => CLOUDINARY_API_SECRET
+  ],
+  'url' => [
+    'secure' => true
+  ]
+]);
+
+return new UploadApi();
