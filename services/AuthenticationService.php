afe19637a317fa4c17d6e06d4f574ef59646fc93
Marco <magp2606gmail.com>
2025-07-10 17:41:52 -0500

    Feat: Añadir servicio de autenticacion para empezar la implementacion del login del panel de control

---
+++ services/AuthenticationService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Services;
+
+use Interfaces\AuthenticationServicieInterface;
+use Interfaces\UserRepositoryInterface;
+
+use Models\Role;
+
+class AuthenticationService implements AuthenticationServicieInterface
+{
+  private $userRepository;
+  public function __construct(UserRepositoryInterface $userRepository)
+  {
+    $this->userRepository = $userRepository;
+  }
+
+  public function login(string $username, string $password): bool
+  {
+    $user = $this->userRepository->getByUsername($username);
+    if ($user && password_verify($password, $user->password)) {
+      $_SESSION['user'] = $user;
+      return true;
+    }
+    return false;
+  }
+
+  public function logout(): void
+  {
+    unset($_SESSION['user']);
+  }
+
+  public function isAuthenticated(): bool
+  {
+    return isset($_SESSION['user']);
+  }
+
+  public function getCurrentUserId(): ?int
+  {
+    return $_SESSION['user']->id;
+  }
+
+  public function getRole(): ?Role
+  {
+    return $_SESSION["user"]->role ?? null;
+  }
+}
